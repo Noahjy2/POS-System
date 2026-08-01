192fcd777446787b46b99fa973078d9eec05843e
David Heng <hengjinyongd@gmail.com>
2026-08-01 11:11:32 +0000

    Implement order file read and save features

--- FileManager.java
+++ FileManager.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.util.ArrayList;
-
+import java.io.BufferedWriter;
 
 public class FileManager {
 
@@ -69,7 +69,102 @@ public class FileManager {
         }
     }
 
-    public void saveCartItem(ArrayList<CartItem> cartItems){
-        
+
+    
+    public void saveOrder(Order order) {
+
+        String filePath = "orders.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+
+            writer.write(order.getId());
+            writer.newLine();
+
+            for (CartItem item : order.getCart()) {
+
+                writer.write(
+                        item.getProduct().getName() + ","
+                        + item.getQuantity() + ","
+                        + item.getProduct().getPrice());
+
+                writer.newLine();
+            }
+
+            writer.write("Total," + order.getTotal());
+
+            writer.newLine();
+            writer.newLine();
+
+        } catch (IOException e) {
+            System.out.println("Cannot save order.");
+        }
+    }
+
+
+
+    public ArrayList<Order> readOrders() {
+
+        ArrayList<Order> orders = new ArrayList<>();
+
+        String filePath = "orders.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+
+            Order currentOrder = null;
+
+            ArrayList<CartItem> cart = null;
+
+            while ((line = reader.readLine()) != null) {
+
+                if (line.isBlank()) {
+                    continue;
+                }
+
+                if (line.startsWith("Order")) {
+
+                    cart = new ArrayList<>();
+
+                    currentOrder = new Order(line, cart, 0);
+
+                }
+                else if (line.startsWith("Total")) {
+
+                    String[] data = line.split(",");
+
+                    double total = Double.parseDouble(data[1]);
+
+                    currentOrder.setTotal(total);
+
+                    orders.add(currentOrder);
+
+                }
+                else {
+
+                    String[] data = line.split(",");
+
+                    String name = data[0];
+
+                    int quantity = Integer.parseInt(data[1]);
+
+                    double price = Double.parseDouble(data[2]);
+
+                    Product product = new Product("", name, price, 0);
+
+                    CartItem item = new CartItem(product, quantity);
+
+                    cart.add(item);
+                }
+
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("Cannot read orders.");
+
+        }
+
+        return orders;
     }
 }
